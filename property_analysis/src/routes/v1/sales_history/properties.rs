4ac0ddacfc5b35e34cbbd4cff20b4a0b484e35e5
dagancode <dagancode@gmail.com>
2026-05-18 20:06:30 +0200

    feat: add RwLock posion handling and warning logging

--- property_analysis/src/routes/v1/sales_history/properties.rs
+++ property_analysis/src/routes/v1/sales_history/properties.rs
@@ -2,7 +2,7 @@ use axum::{
     Json, extract::{Query, State}, http::StatusCode
 };
 use serde::{Deserialize, Serialize};
-use tracing::debug;
+use tracing::{debug, warn};
 
 use crate::models::{app::AppState, domain::PropertyDetail, error::ApiError};
 
@@ -23,10 +23,21 @@ pub async fn get_property_sales_history(
     let street = property_request.street.to_lowercase();
     let number = property_request.number.to_lowercase();
 
-    match state.sales_history.read() {
-        Ok(lock) => {
-            let result: Vec<PropertyDetail> = lock
-                .iter()
+    let guard = match state.sales_history.read() {
+        Ok(lock) => lock,
+        Err(poison_error) => {
+            let lock = poison_error.into_inner();
+            warn!(
+                "Lock was poisoned - recovering from last stable state ({} items)",
+                lock.len()
+            );
+
+            lock
+        }
+    };
+
+    let result: Vec<PropertyDetail> = guard
+        .iter()
                 .filter(|val| {
                     val.property.location.suburb.eq(&suburb)
                         && val.property.location.street_name.contains(&street)
@@ -39,15 +50,14 @@ pub async fn get_property_sales_history(
                 .cloned()
                 .collect();
 
-            if result.is_empty() {
-                debug!("{} -> {}", format!("GET /sales-history/properties?suburb={suburb}&street={street}&number={number}"), StatusCode::NOT_FOUND);
-                return Err(ApiError::NotFound);
-            }
-
-            debug!("{} -> {}", format!("GET /sales-history/properties?suburb={suburb}&street={street}&number={number}"), StatusCode::OK);
-            Ok(Json(result))
-        }
-
-        Err(_) => Err(ApiError::PoisonedLock),
+    if result.is_empty() {
+        debug!(
+            "GET /sales-history/suburb/{suburb} -> {}",
+            StatusCode::NOT_FOUND
+        );
+        return Err(ApiError::NotFound);
     }
+
+    debug!("GET /sales-history/suburb/{suburb} -> {}", StatusCode::OK);
+    Ok(Json(result))
 }
--- property_analysis/src/routes/v1/sales_history/suburbs.rs
+++ property_analysis/src/routes/v1/sales_history/suburbs.rs
@@ -1,7 +1,9 @@
 use axum::{
-    Json, extract::{Path, State}, http::StatusCode
+    extract::{Path, State},
+    http::StatusCode,
+    Json,
 };
-use tracing::debug;
+use tracing::{debug, warn};
 
 use crate::models::{app::AppState, domain::PropertyDetail, error::ApiError};
 
@@ -9,23 +11,33 @@ pub async fn get_suburb_sales_history(
     Path(suburb): Path<String>,
     State(state): State<AppState>,
 ) -> Result<Json<Vec<PropertyDetail>>, ApiError> {
-    match state.sales_history.read() {
-        Ok(lock) => {
-            let result: Vec<PropertyDetail> = lock
-                .iter()
-                .filter(|val| val.property.location.suburb.contains(&suburb))
-                .cloned()
-                .collect();
+    let guard = match state.sales_history.read() {
+        Ok(lock) => lock,
+        Err(poison_error) => {
+            let lock = poison_error.into_inner();
+            warn!(
+                "Lock was poisoned - recovering from last stable state ({} items)",
+                lock.len()
+            );
 
-            if result.is_empty() {
-                debug!("{} -> {}", format!("GET /sales-history/suburb/{suburb}"), StatusCode::NOT_FOUND);
-                return Err(ApiError::NotFound);
-            }
-
-            debug!("{} -> {}", format!("GET /sales-history/suburb/{suburb}"), StatusCode::OK);
-            Ok(Json(result))
+            lock
         }
+    };
 
-        Err(_) => Err(ApiError::PoisonedLock),
+    let result: Vec<PropertyDetail> = guard
+        .iter()
+        .filter(|val| val.property.location.suburb.contains(&suburb))
+        .cloned()
+        .collect();
+
+    if result.is_empty() {
+        debug!(
+            "GET /sales-history/suburb/{suburb} -> {}",
+            StatusCode::NOT_FOUND
+        );
+        return Err(ApiError::NotFound);
     }
+
+    debug!("GET /sales-history/suburb/{suburb} -> {}", StatusCode::OK);
+    Ok(Json(result))
 }
